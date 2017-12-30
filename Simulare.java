48fd58a28f217ac026eb40ef67fcf58bba5c6d26
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-12-30 17:57:24 +0200

    Update Simulare.java

--- Simulare.java
+++ Simulare.java
@@ -1,12 +1,57 @@
+import java.util.Iterator;
+
+import static java.lang.Character.MAX_VALUE;
+
 /**
  * Created by ioan on 12/29/17.
  */
 public class Simulare
 {
+    private static final int NR_CLIENTI_INAINDE_DE_DESCHIDERE = 20;
     public static AgentiePostala PostaRomana;
+    public static int clock;
     Simulare()
     {
+        Read.console();
+    }
 
+    void start()
+    {
+        int int_temp;
+        Client Clienti_temp[];
+        Generator G;
+        GenerareClient GC;
+        Ghiseu Ghiseu_temp;
+        G = new StdGenerator(TimpConvert.toInt(PostaRomana.timpdeschidere));
+        GC = new GenerareClient(G);
+        do{
+            int_temp=G.next()+1;
+        }while (int_temp>NR_CLIENTI_INAINDE_DE_DESCHIDERE);
+        Clienti_temp = new Client[int_temp];
+        for(int i=0;i<=Clienti_temp.length; i++){
+            Clienti_temp[i]= GC.generare();
+        }
+        clock=0;
+        while (clock<=TimpConvert.toInt(PostaRomana.timpinchidere)){
+            for(int i=0; i<Clienti_temp.length; i++){
+                for(int j=0; j<Clienti_temp[i].Get_nroperatiuni(); j++){
+                    int index=0;
+                    int min=MAX_VALUE;
+                    Iterator<Ghiseu> iterator = PostaRomana.Get_lista_ghisee();
+                    while (iterator.hasNext()){
+                        Ghiseu_temp = iterator.next();
+                        if(Ghiseu_temp.contine_operatiune_id(Clienti_temp[i].Get_operatiune_index(j).Get_operatiune().Get_id())==true){
+                            if(Ghiseu_temp.Get_sizecoada_clienti()<min){
+                                min = Ghiseu_temp.Get_sizecoada_clienti();
+                                index = PostaRomana.Get_idGhisu(Ghiseu_temp);
+                            }
+                        }
+                    }
+                    PostaRomana.Get_ghisu(index).Push_coada_clienti(Clienti_temp[i]);
+                }
+            }
+            clock++;
+        }
     }
 
 }
