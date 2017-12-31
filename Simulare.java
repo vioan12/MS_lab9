d71c3e58cfd44e99f6635a88d6c67a401851052d
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-12-31 10:24:20 +0200

    Update Simulare.java

--- Simulare.java
+++ Simulare.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 
 import static java.lang.Character.MAX_VALUE;
+import static java.lang.Character.isISOControl;
 
 /**
  * Created by ioan on 12/29/17.
@@ -49,6 +50,34 @@ public class Simulare
                     }
                     PostaRomana.Get_ghisu(index).Push_coada_clienti(Clienti_temp[i]);
                 }
+            }
+
+            Iterator<Ghiseu> iterator = PostaRomana.Get_lista_ghisee();
+            while (iterator.hasNext()){
+                Ghiseu_temp = iterator.next();
+                if(PostaRomana.ClientOperatiuneInDesfasurare(Ghiseu_temp.Head_coada_clienti())==false){ //Clientul nu are nici o operatiune in desfasurare
+                    boolean sw=true;
+                    for(int i=0; i<Ghiseu_temp.Head_coada_clienti().Get_nroperatiuni(); i++){
+                        if(Ghiseu_temp.contine_operatiune_id(Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Get_operatiune().Get_id())==true){ //Giseul poate efectua operatiunea i a Clientului
+                            if(Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Get_sw_executata()==false){ //Operatiunea i nu a fost executata
+                                sw=false;
+                                if(Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Get_sw_desfasurare()==false){ //Operatiunea i nu este in desfasurare
+                                    Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Desfasurareoperatiune();
+                                    Generator G_timpdesfasurareoperatiune;
+                                    G_timpdesfasurareoperatiune = new StdGenerator(Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Get_operatiune().Get_timpmax() - Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Get_operatiune().Get_timpmin()+1);
+                                    Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Set_timpexec(G_timpdesfasurareoperatiune.next()+Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).Get_operatiune().Get_timpmin());
+                                } else {
+                                    //if(Ghiseu_temp.Head_coada_clienti().Get_operatiune_index(i).)
+                                }
+
+                            } else {
+
+                            }
+                        }
+                    }
+                    if(sw==true){ //Toate operatiunile au fost executate
+                        Ghiseu_temp.Pop_coada_clienti();
+                    }
             }
             clock++;
         }
