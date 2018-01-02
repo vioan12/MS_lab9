e3db211559e325958e5fcbf5bd7954a2ca9d5cb9
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2018-01-02 17:31:49 +0200

    Update ClientComparator.java

--- ClientComparator.java
+++ ClientComparator.java
@@ -7,13 +7,32 @@ public class ClientComparator implements Comparator<Client>
 {
     @Override public int compare(Client o1, Client o2)
     {
-        if (o1.Get_timpsosire() < o2.Get_timpsosire())
-        {
-            return -1;
+        boolean sw_o1=false,sw_o2=false;
+        for(int i=0; i<(o1.Get_nroperatiuni())&&(sw_o1==false); i++){
+            if(o1.Get_operatiune_index(i).Get_sw_desfasurare()==true){
+                sw_o1=true;
+            }
         }
-        if (o1.Get_timpsosire() > o2.Get_timpsosire())
-        {
-            return 1;
+        for(int i=0; i<(o2.Get_nroperatiuni())&&(sw_o2==false); i++){
+            if(o2.Get_operatiune_index(i).Get_sw_desfasurare()==true){
+                sw_o2=true;
+            }
+        }
+        if(sw_o1 != sw_o2) {
+            if ((sw_o1 == false) && (sw_o2 == true)) {
+                return -1;
+            }
+            if ((sw_o1 == true) && (sw_o2 == false)) {
+                return 1;
+            }
+        }
+        if(sw_o1 == sw_o2) {
+            if (o1.Get_timpsosire() < o2.Get_timpsosire()) {
+                return -1;
+            }
+            if (o1.Get_timpsosire() > o2.Get_timpsosire()) {
+                return 1;
+            }
         }
         return 0;
     }
